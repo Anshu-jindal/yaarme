4530a9bcf260cdfc982f18a1ef5c62a877cb815c
Anshu Jindal <anshujindal989@gmail.com>
2020-12-10 17:52:04 +0530

    added header.js for search icon and company title

---
+++ header.js
@@ -0,0 +1,8 @@
+function invisible() {
+  document.querySelector("#company-title").classList.add("invisible");
+  document.querySelector("#search-icon-img").classList.add("invisible");
+  document.querySelector("#search_mob").classList.remove("invisible");
+  document
+    .querySelector("#header-form")
+    .classList.remove("blue-background-header");
+}
